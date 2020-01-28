a5583edd99bf961461312016b01ca5d02e40e85d
LiuPeiWen0140 <19cm0140@jec.ac.jp>
2020-01-28 18:23:54 +0900

    Initial Commit

---
+++ BetterPayBack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BetterPayBack
+//
+//  Created by cmStudent on 2020/01/28.
+//  Copyright © 2020 19cm0140. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
